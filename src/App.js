32d29697acbbeff9c43c46d97d1c8bd68cb0f4c6
Pamir Perdeci <pamirperdeci@gmail.com>
2023-01-18 17:38:28 -0500

    Added a button and changed the background color using styled components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
 import PhotoOfDay from "./components/PhotoOfDay";
+import styled from "styled-components";
 
 
 
 
 function App() {
+  const Button = styled.button`
+  background-color: green;
+  padding: 20px;
+  margin-bottom: 200px;
+  
+  `;
 
+  const Background = styled.div`
+    background-color: lightblue;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    margin: 0 200px;
+    padding: 0 200px;
+
+  `
 
   const [data, setData] = useState("https://www.nasa.gov/sites/default/files/thumbnails/image/1_icon_olympus_multi-purpose_isru-based_lunarconstructionsystem_concept-render_nov2022_for_icon_release.jpg")
   
@@ -24,9 +40,10 @@ function App() {
 
 
   return (
-    <div className="App" style={{display:"flex", flexDirection:"column",alignItems:"center"}}>
+    <Background className="App" >
       <PhotoOfDay data={data} />
-    </div>
+      <Button>Click Me</Button>
+    </Background>
   );
 }
 
